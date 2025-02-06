32809a4cbdc46d3f70efe780a3b86dafe6f462c0
Dan Smith <dev@dansmith.tech>
2025-02-06 16:02:35 +0100

    Change nvim CodeCompanion to Deekseek R1 32b with Ollama

--- stow/.config/nvim/lua/plugins/assistant.lua
+++ stow/.config/nvim/lua/plugins/assistant.lua
@@ -1,31 +1,43 @@
 return {
-   "olimorris/codecompanion.nvim",
-   dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-   },
-   config = function()
-      require("codecompanion").setup({
-         strategies = {
-            chat = {
-               adapter = "anthropic",
-            },
-            inline = {
-               adapter = "anthropic",
-            },
-         },
-         display = {
-            action_palette = {
-               provider = "telescope",
-            },
-         },
-      })
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("codecompanion").setup({
+			adapters = {
+				llama3 = function()
+					return require("codecompanion.adapters").extend("ollama", {
+						name = "deepseek-r1",
+						schema = {
+							model = {
+								default = "deepseek-r1:32b",
+							},
+						},
+					})
+				end,
+			},
+			strategies = {
+				chat = {
+					adapter = "ollama",
+				},
+				inline = {
+					adapter = "ollama",
+				},
+			},
+			display = {
+				action_palette = {
+					provider = "telescope",
+				},
+			},
+		})
 
-      -- Expand 'cc' into 'CodeCompanion' in the command line
-      vim.cmd([[cab cc CodeCompanion]])
-      -- Expand 'ccc' into 'CodeCompanionChat Toggle' in the command line
-      vim.cmd([[cab ccc CodeCompanionChat Toggle]])
-      -- Expand 'cca' into 'CodeCompanionActions' in the command line
-      vim.cmd([[cab cca CodeCompanionActions]])
-   end,
+		-- Expand 'cc' into 'CodeCompanion' in the command line
+		vim.cmd([[cab cc CodeCompanion]])
+		-- Expand 'ccc' into 'CodeCompanionChat Toggle' in the command line
+		vim.cmd([[cab ccc CodeCompanionChat Toggle]])
+		-- Expand 'cca' into 'CodeCompanionActions' in the command line
+		vim.cmd([[cab cca CodeCompanionActions]])
+	end,
 }
