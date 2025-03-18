7fdddd320d1b3088ab37674077186f6f7237acad
Dan Smith <dev@dansmith.tech>
2025-03-18 14:39:39 +0100

    Add conflict finder

--- stow/.config/nvim/lua/plugins/git.lua
+++ stow/.config/nvim/lua/plugins/git.lua
@@ -1,10 +1,17 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup({
-			diff_opts = {
-				internal = true,
-			},
-		})
-	end,
+   {
+      "lewis6991/gitsigns.nvim",
+      config = function()
+         require("gitsigns").setup({
+            diff_opts = {
+               internal = true,
+            },
+         })
+      end,
+   },
+   {
+      "akinsho/git-conflict.nvim",
+      tag = "*",
+      config = true,
+   },
 }
