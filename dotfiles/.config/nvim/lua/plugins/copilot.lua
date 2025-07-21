957827de43ce4856a3bf2d4f61c396d8b0c509c7
Daniel James Smith <daniel.james.smith@cern.ch>
2025-07-21 11:07:48 +0200

    stop tab from inserting AI slop

--- dotfiles/.config/nvim/lua/plugins/copilot.lua
+++ dotfiles/.config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,12 @@
 return {
    "github/copilot.vim",
    -- Note: must run `:Copilot setup` after this package gets installed
+   config = function()
+      -- Use Shift+Tab to accept suggestions
+      vim.keymap.set("i", "<S-Tab>", 'copilot#Accept("\\<CR>")', {
+         expr = true,
+         replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+   end,
 }
