34503e3d957a97bbf74c63b9d521ded28c611da4
Dan Smith <dev@dansmith.tech>
2026-05-02 04:01:47 +0100

    simplify nvim config after switching to zed

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -1,57 +1,28 @@
 vim.g.mapleader = " "
 
--- Enable line numbers
+-- enable line numbers
 vim.opt.number = true
--- Use spaces instead of tabs
+-- use spaces instead of tabs
 vim.opt.expandtab = true
 
--- Enable spell checking for UK and US English
+-- enable spell checking for UK and US English
 vim.opt.spell = true
 vim.opt.spelllang = { "en_us", "en_gb" }
 
--- A <Tab> counts as 3 spaces
+-- make a <Tab> counts as 3 spaces
 vim.opt.tabstop = 3
 vim.opt.softtabstop = 3
 vim.opt.shiftwidth = 3
 
--- Move left and right between windows using Ctrl + h and Ctrl + l
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
--- Move up and down between windows using Ctrl + j and Ctrl + k
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
-
--- Disable settings in terminal windows
-vim.api.nvim_create_autocmd("TermOpen", {
-	pattern = "*",
-	callback = function()
-		vim.opt_local.number = false
-		vim.opt_local.relativenumber = false
-		vim.opt_local.spell = false
-	end,
-})
-
--- Automatically equalize all window widths and heights on resize
-vim.api.nvim_create_autocmd("VimResized", {
-   pattern = "*",
-   callback = function()
-      vim.cmd("wincmd =")
-   end,
-})
-
--- If Lazy (package manager) has not been cloned, clone it
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+-- download and set catppuccin mocha theme
+local catpath = vim.fn.stdpath("data") .. "/site/pack/plugins/start/catppuccin"
+if not vim.uv.fs_stat(catpath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/catppuccin/nvim",
+        catpath,
+    })
+    vim.cmd("packadd catppuccin")
 end
-vim.opt.rtp:prepend(lazypath)
-
--- Setup Lazy to load plugins from each of the individual lua/plugins/<plugin>.lua files
-require("lazy").setup("plugins")
+require("catppuccin").setup({ flavour = "mocha" })
+vim.cmd.colorscheme("catppuccin")
