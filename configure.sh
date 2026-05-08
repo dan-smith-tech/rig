adec8b78ed54ed1b79bf8578a74f4ebc20634ec3
Dan Smith <dev@dansmith.tech>
2026-05-08 23:29:21 +0100

    remove unused packages and setup virtual keyboard

--- configure.sh
+++ configure.sh
@@ -29,20 +29,6 @@ yay -S --noconfirm brave-bin catppuccin-plasma-colorscheme-mocha
 
 sudo chsh -s /usr/bin/zsh "$CURRENT_USER"
 
-ssh-keygen -t rsa -b 4096 -f "$HOME/.ssh/id_rsa" -N ""
-
-read -p "Git email: " git_email
-git config --global user.email "$git_email"
-
-read -p "Git name: " git_name
-git config --global user.name "$git_name"
-
-gh auth login
-
-rustup default stable
-
-sudo usermod -aG docker "$CURRENT_USER"
-
 cd "$HOME/rig/dotfiles"
 stow --adopt -t ~ nvim kitty zshrc zed gitignore
 git restore .
@@ -53,15 +39,14 @@ git config --global core.excludesfile '~/.gitignore_global'
 git config --global core.editor nvim
 git config --global diff.tool kitty
 git config --global difftool.kitty.cmd 'kitten diff $LOCAL $REMOTE'
+read -p "Git email: " git_email
+git config --global user.email "$git_email"
+read -p "Git name: " git_name
+git config --global user.name "$git_name"
+gh auth login
 
-sudo timedatectl set-timezone "Europe/London"
-sudo timedatectl set-ntp true
+rustup default stable
 
-sudo pacman -S --noconfirm cups cups-pdf system-config-printer print-manager
-sudo systemctl enable --now cups.service
-sudo usermod -aG lp "$CURRENT_USER"
-
-yay -S --noconfirm xone-dkms-git xone-dongle-firmware linux-headers
-sudo mkinitcpio -P
+echo 'KWIN_IM_SHOW_ALWAYS=1' | sudo tee -a /etc/environment
 
 sudo reboot
